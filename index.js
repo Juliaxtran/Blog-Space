7c04ef2d95c975615c97a53734e6fca0c8a8cca0
Julia Tran <julia.kim.tran@hotmail.com>
2022-12-05 18:51:41 -0500

    Blog Posts

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
 .then(res => res.json())
 .then(data => {
- let blogPost =  data.slice(0, 5)
- console.log(blogPost);
-} )
+ let blogPosts =  data.slice(0, 5)
+ let html = '';
+ for (let post of blogPosts) {
+  html += `
+  <h3>${post.title}</h3>
+  <p>${post.body}</p>
+  `
+ }
+ document.getElementById("blog-list").innerHTML = html;
+;} )
