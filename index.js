9ee9267a1c85c09f538448d7d289284c67c498b6
Julia Tran <julia.kim.tran@hotmail.com>
2022-12-07 19:16:31 -0500

    Comments

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
-.then(res => res.json())
-.then(data => {
- let blogPosts =  data.slice(0, 5)
- let html = '';
- for (let post of blogPosts) {
-  html += `
-  <h3>${post.title}</h3>
-  <p>${post.body}</p>
-  <hr />
+  .then(res => res.json())
+  .then(data => {
+    let blogPosts = data.slice(0, 5)
+    let html = '';
+    for (let post of blogPosts) {
+      html += `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <hr />
   `
- }
- document.getElementById("blog-list").innerHTML = html;
-;} )
+    }
+    document.getElementById("blog-list").innerHTML = html;
+    ;
+  })
+
+
+
+document.getElementById("new-post").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const postTitle = document.getElementById("post-title").value;
+  const postBody = document.getElementById("post-body").value;
+  const data = {
+    title: postTitle,
+    body: postBody
+  }
+
+})
+
+// Comments
